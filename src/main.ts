2e2dbcbd7a388a0d3641e6a06eee67e3d056c3fc
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-23 12:42:52 -0500

    also log the ref

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-import {wait} from './wait'
-
 async function run(): Promise<void> {
   try {
     core.debug(`repo is ${github.context.repo}`)
-
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`ref is ${github.context.ref}`)
   } catch (error) {
     core.setFailed(error.message)
   }
