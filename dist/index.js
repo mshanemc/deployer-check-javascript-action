0a0010dc48cc6d2352d4959b56d6dea35215e891
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-23 17:11:37 -0500

    packaged into dist

--- dist/index.js
+++ dist/index.js
@@ -6277,15 +6277,18 @@ const retryOptions = {
     initialDelay: 10000,
     maxAttempts: 6 * 15
 };
+const defaultBranchName = 'master';
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         let baseUrl = core.getInput('deployer-url').length > 0
             ? core.getInput('deployer-url')
             : 'https://hosted-scratch-dev.herokuapp.com';
         try {
-            // todo: handle branches with ref
             console.log(`ref is ${github.context.ref}`);
-            const launchUri = `${baseUrl}/launch?template=https://github.com/${github.context.repo.owner}/${github.context.repo.repo}&nopool=true`;
+            const branch = github.context.ref.replace('refs/heads/', '');
+            const launchUri = branch === defaultBranchName
+                ? `${baseUrl}/launch?template=https://github.com/${github.context.repo.owner}/${github.context.repo.repo}&nopool=true`
+                : `${baseUrl}/launch?template=https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/tree/${branch}&nopool=true`;
             let resultsUri;
             let deployId = '';
             console.log(launchUri);
